964a94e75d74da69e0dabd865b15da85fb03e3d0
ozikot <dendai0122@gmail.com>
2019-04-29 17:57:00 +0900

    add headher(toolber) & change color(main/text) lonk determination unsolved

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,19 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <global-header/>
     <router-view/>
   </div>
 </template>
 
 <script>
+import GlobalHeader from './components/GlobalHeader'
 export default {
-  name: 'App'
+  components: {
+    GlobalHeader,
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-toolbar color="#4d4398">
+    <v-toolbar-side-icon class="white--text"></v-toolbar-side-icon>
+    <v-toolbar-title class="white--text">cp-categorize beta</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-items >
+      <v-btn flat class="white--text">
+        <a class="nav-link" style="color: #fff" href="https://github.com/ozikot" target="_blank"><span class="nav">GitHub</span></a>
+      </v-btn>
+      <v-btn flat class="white--text">
+        <a class="nav-link" style="color: #fff" href="https://beta.atcoder.jp/users/tokizo" target="_blank"><span class="nav">AtCoder</span></a>
+      </v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
+
+<<script>
+export default {
+  props: {
+    color: "purple"
+  }
+}
+</script>
+
+<style scoped>
+*{
+  text-transform: none !important;
+  text-decoration: none;
+}
+</style>
