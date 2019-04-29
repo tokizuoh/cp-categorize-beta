4aea09b74a1e1bb57227cb02f5a32ce93949281e
ozikot <dendai0122@gmail.com>
2019-04-29 20:58:08 +0900

    add Header

--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -1,20 +1,20 @@
 <template>
-  <v-toolbar color="#4d4398">
+  <v-toolbar color="#4d4398" height=47>
     <v-toolbar-side-icon class="white--text"></v-toolbar-side-icon>
     <v-toolbar-title class="white--text">cp-categorize beta</v-toolbar-title>
     <v-spacer></v-spacer>
     <v-toolbar-items >
-      <v-btn flat class="white--text">
-        <a class="nav-link" style="color: #fff" href="https://github.com/ozikot" target="_blank"><span class="nav">GitHub</span></a>
+      <v-btn flat class="white--text" href="https://github.com/ozikot">
+        GitHub
       </v-btn>
-      <v-btn flat class="white--text">
-        <a class="nav-link" style="color: #fff" href="https://beta.atcoder.jp/users/tokizo" target="_blank"><span class="nav">AtCoder</span></a>
+      <v-btn flat class="white--text" href="https://beta.atcoder.jp/users/tokizo">
+        AtCoder
       </v-btn>
     </v-toolbar-items>
   </v-toolbar>
 </template>
 
-<<script>
+<script>
 export default {
   props: {
     color: "purple"
